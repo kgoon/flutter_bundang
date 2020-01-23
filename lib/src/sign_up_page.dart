b4702441564bf5ee0d493c8cb42e60b4c6100a75
PLATO JUNG <elephamaximu@gmail.com>
2020-01-23 10:47:57 +0900

    SingleChildScrollView 추가 후 스크린 여백 재정리

--- lib/src/sign_up_page.dart
+++ lib/src/sign_up_page.dart
@@ -14,87 +14,92 @@ class _SignUpPageState extends State<SignUpPage> {
     return Scaffold(
       body: Container(
         margin: EdgeInsets.all(10.0),
-        child: SingleChildScrollView(
+        child: Center(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  'Awesome App',
-                  style: TextStyle(
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey[700],
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    'Awesome App',
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[700],
+                    ),
                   ),
-                ),
-                Column(
-                  children: <Widget>[
-                    Form(
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'email',
-                            ),
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'password',
-                            ),
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'password confirm',
-                            ),
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'birthday',
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20.0,
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Checkbox(
-                                value: _ifChecked,
-                                onChanged: (bool value) {
-                                  setState(() {
-                                    _ifChecked = value;
-                                  });
-                                },
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 7,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Form(
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'email',
                               ),
-                              Text('I accept the terms of service'),
-                            ],
-                          ),
-                        ],
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'password',
+                              ),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'password confirm',
+                              ),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'birthday',
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Checkbox(
+                                  value: _ifChecked,
+                                  onChanged: (bool value) {
+                                    setState(() {
+                                      _ifChecked = value;
+                                    });
+                                  },
+                                ),
+                                Text('I accept the terms of service'),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    RaisedButton(
-                      onPressed: () {},
-                      color: Colors.teal,
-                      textColor: Colors.white,
-                      child: Text(
-                        'Sign Up'.toUpperCase(),
+                      SizedBox(
+                        height: 30.0,
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      RaisedButton(
+                        onPressed: () {},
+                        color: Colors.teal,
+                        textColor: Colors.white,
+                        child: Text(
+                          'Sign Up'.toUpperCase(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
