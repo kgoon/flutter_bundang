fc4dbfd1d06978ee4a85815c7b91f7cb873570b5
PLATO JUNG <elephamaximu@gmail.com>
2020-01-23 10:10:51 +0900

    회원 가입 폼 뼈대 작성

--- lib/src/sign_up_page.dart
+++ lib/src/sign_up_page.dart
@@ -8,12 +8,54 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  bool _ifChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        margin: EdgeInsets.all(10.0),
         child: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            Form(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'email',
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'password',
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'password confirm',
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'birthday',
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Checkbox(
+                        value: _ifChecked,
+                        onChanged: (bool value) {
+                          setState(() {
+                            _ifChecked = value;
+                          });
+                        },
+                      ),
+                      Text('I accept the terms of service'),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
