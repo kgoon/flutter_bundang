0cd580e725811be4c7c8989d1c2f9dcb19ba21f0
PLATO JUNG <elephamaximu@gmail.com>
2020-01-24 00:56:33 +0900

    button 패딩 추가, sizedbox -> 패딩 변경

--- lib/src/sign_up_page.dart
+++ lib/src/sign_up_page.dart
@@ -36,128 +36,137 @@ class _SignUpPageState extends State<SignUpPage> {
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height / 7,
-                  ),
                   Column(
                     children: <Widget>[
                       Form(
                         child: Column(
                           children: <Widget>[
-                            TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'email',
-                                labelStyle: TextStyle(
-                                  fontSize:
-                                      MediaQuery.of(context).size.width > 700
-                                          ? 26.0
-                                          : 16.0,
-                                ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 80.0,
                               ),
-                            ),
-                            SizedBox(
-                              height: 8,
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'password',
-                                labelStyle: TextStyle(
-                                  fontSize:
-                                      MediaQuery.of(context).size.width > 700
-                                          ? 26.0
-                                          : 16.0,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 8,
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'password confirm',
-                                labelStyle: TextStyle(
-                                  fontSize:
-                                      MediaQuery.of(context).size.width > 700
-                                          ? 26.0
-                                          : 16.0,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 8,
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                labelText: _selectedDate == null
-                                    ? 'birthday'
-                                    : DateFormat.yMd('ko')
-                                        .format(_selectedDate),
-                                labelStyle: TextStyle(
-                                  fontSize:
-                                      MediaQuery.of(context).size.width > 700
-                                          ? 26.0
-                                          : 16.0,
-                                ),
-                              ),
-                              onTap: () {
-                                FocusScope.of(context)
-                                    .requestFocus(new FocusNode());
-                                showDatePicker(
-                                        context: context,
-                                        locale: const Locale('ko'),
-                                        firstDate: DateTime.now()
-                                            .subtract(Duration(days: 36500)),
-                                        initialDate: DateTime.now()
-                                            .subtract(Duration(days: 3650)),
-                                        lastDate: DateTime.now(),
-                                        initialDatePickerMode:
-                                            DatePickerMode.year)
-                                    .then((onValue) {
-                                  if (onValue == null) {
-                                    return;
-                                  } else {
-                                    setState(() {
-                                      _selectedDate = onValue;
-                                    });
-                                  }
-                                });
-                              },
-                            ),
-                            SizedBox(
-                              height: 20.0,
-                            ),
-                            Transform.scale(
-                              scale: 1.06,
-                              child: CheckboxListTile(
-                                value: _ifChecked,
-                                onChanged: (bool value) {
-                                  setState(() {
-                                    _ifChecked = value;
-                                  });
-                                },
-                                title: Text(
-                                  'I accept the terms of service',
-                                  style: TextStyle(
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'email',
+                                  labelStyle: TextStyle(
                                     fontSize:
                                         MediaQuery.of(context).size.width > 700
-                                            ? 23.0
-                                            : 14.0,
+                                            ? 26.0
+                                            : 16.0,
                                   ),
                                 ),
-                                controlAffinity:
-                                    ListTileControlAffinity.leading,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(
+                                8.0,
+                              ),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'password',
+                                  labelStyle: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.width > 700
+                                            ? 26.0
+                                            : 16.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 8.0,
+                              ),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'password confirm',
+                                  labelStyle: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.width > 700
+                                            ? 26.0
+                                            : 16.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 8.0,
+                              ),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: _selectedDate == null
+                                      ? 'birthday'
+                                      : DateFormat.yMd('ko')
+                                          .format(_selectedDate),
+                                  labelStyle: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.width > 700
+                                            ? 26.0
+                                            : 16.0,
+                                  ),
+                                ),
+                                onTap: () {
+                                  FocusScope.of(context)
+                                      .requestFocus(new FocusNode());
+                                  showDatePicker(
+                                          context: context,
+                                          locale: const Locale('ko'),
+                                          firstDate: DateTime.now()
+                                              .subtract(Duration(days: 36500)),
+                                          initialDate: DateTime.now()
+                                              .subtract(Duration(days: 3650)),
+                                          lastDate: DateTime.now(),
+                                          initialDatePickerMode:
+                                              DatePickerMode.year)
+                                      .then((onValue) {
+                                    if (onValue == null) {
+                                      return;
+                                    } else {
+                                      setState(() {
+                                        _selectedDate = onValue;
+                                      });
+                                    }
+                                  });
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20.0),
+                              child: Transform.scale(
+                                scale: 1.06,
+                                child: CheckboxListTile(
+                                  value: _ifChecked,
+                                  onChanged: (bool value) {
+                                    setState(() {
+                                      _ifChecked = value;
+                                    });
+                                  },
+                                  title: Text(
+                                    'I accept the terms of service',
+                                    style: TextStyle(
+                                      fontSize:
+                                          MediaQuery.of(context).size.width >
+                                                  700
+                                              ? 23.0
+                                              : 14.0,
+                                    ),
+                                  ),
+                                  controlAffinity:
+                                      ListTileControlAffinity.leading,
+                                ),
                               ),
                             ),
                           ],
                         ),
                       ),
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width / 2.4,
-                        height: MediaQuery.of(context).size.height / 14,
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30.0),
                         child: RaisedButton(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 20.0,
+                            horizontal: 40.0,
+                          ),
                           onPressed: _ifChecked ? () {} : null,
                           color: Colors.teal,
                           textColor: Colors.white,
