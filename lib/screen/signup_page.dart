f574db843651f24ab1a078856e07e8f6d9373a81
seungkil choi <seung-kil.choi@raizegls.com>
2020-01-28 11:39:24 +0900

    회원가입 Form 위젯 분리

--- lib/screen/signup_page.dart
+++ lib/screen/signup_page.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class SignupPage extends StatefulWidget {
+class SignupPage extends StatelessWidget {
   SignupPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _SignupPageState createState() => _SignupPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              title,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            SignupForm()
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _SignupPageState extends State<SignupPage> {
+class SignupForm extends StatefulWidget{
+  @override
+  _SignupFormState createState() => _SignupFormState(); 
+}
 
+
+class _SignupFormState extends State<SignupForm> {
   String _birth;
 
   bool _agreement = false;
@@ -32,82 +53,64 @@ class _SignupPageState extends State<SignupPage> {
       }
   }
 
-  @override
   Widget build(BuildContext context) {
-    
-    _birthController.text = _birth;
-
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              widget.title,
-              style: Theme.of(context).textTheme.display1,
+    return Form(
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'email'
             ),
-            Form(
-              child: Column(
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: 'password'
+            ),
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: 'password confirm'
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              _selectDate();   // Call Function that has showDatePicker()
+            },
+            child: IgnorePointer(
+              child: TextFormField(
+                controller: _birthController,
+                decoration: const InputDecoration(
+                  hintText: 'birthday'
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: (){
+              setState(() {
+                _agreement = !_agreement;
+              });
+            },
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
                 children: <Widget>[
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'email'
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: 'password'
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: 'password confirm'
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      _selectDate();   // Call Function that has showDatePicker()
-                    },
-                    child: IgnorePointer(
-                      child: TextFormField(
-                        controller: _birthController,
-                        decoration: const InputDecoration(
-                          hintText: 'birthday'
-                        ),
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: (){
+                  Checkbox(
+                    value: _agreement,
+                    onChanged: (bool newValue) {
                       setState(() {
-                        _agreement = !_agreement;
+                        _agreement = newValue;
                       });
                     },
-                    child: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Row(
-                        children: <Widget>[
-                          Checkbox(
-                            value: _agreement,
-                            onChanged: (bool newValue) {
-                              setState(() {
-                                _agreement = newValue;
-                              });
-                            },
-                          ),
-                          Expanded(child: Text('I accept the terms of service')),
-                        ],
-                      ),
-                    ),
                   ),
+                  Expanded(child: Text('I accept the terms of service')),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
