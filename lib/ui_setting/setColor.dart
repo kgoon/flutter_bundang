6d39a2505c520b22ffe7f309c1ef9be14880a843
Jack Kim <>
2020-01-29 08:22:30 +0900

    setColor 파일에 색상 결정 setText 파일에 텍스트 설정추가 login페이지

---
+++ lib/ui_setting/setColor.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+
+
+// (1) 폰트 색상 설정 (폰트포함)
+const Color fontColor_50 = Color(0xFFFAFAFA);
+const Color fontColor_100 = Color(0xFFF5F5F5);
+const Color fontColor_200 = Color(0xFFEEEEEE);
+const Color fontColor_300 = Color(0xFFE0E0E0);
+const Color fontColor_400 = Color(0xFFBDBDBD);
+const Color fontColor_500 = Color(0xFF9E9E9E);
+const Color fontColor_600 = Color(0xFF757575);
+const Color fontColor_700 = Color(0xFF616161);
+const Color fontColor_800 = Colors.grey;
+const Color fontColor_900 = Color(0xFF4D70A6);
+
+// (2) 메인 색상 설정 (폰트포함)
+const Color mainColor_50 = Color(0xFFFAFAFA);
+const Color mainColor_100 = Color(0xFFF5F5F5);
+const Color mainColor_200 = Color(0xFFEEEEEE);
+const Color mainColor_300 = Color(0xFFE0E0E0);
+const Color mainColor_400 = Color(0xFFBDBDBD);
+const Color mainColor_500 = Color(0xFF9E9E9E);
+const Color mainColor_600 = Color(0xFF757575);
+const Color mainColor_700 = Color(0xFF616161);
+const Color mainColor_800 = Color(0xFF424242);
+const Color mainColor_900 = Color(0xFF4D70A6);
+
+// (3) 배너색상 설정
+const Color bannerRed = Color(0xFFC62828);
+const Color bannerBlue = Color(0xFF1565C0);
+const Color bannerGreen = Color(0xFF00695C);
+const Color bannerYellow = Color(0xFFFf8F00);
