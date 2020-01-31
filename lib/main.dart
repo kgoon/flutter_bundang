7f7c37d4a1a5ca43967cccd8bd1bc8c5bc147e31
PLATO JUNG <elephamaximu@gmail.com>
2020-01-31 11:13:52 +0900

    screen 폴더 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bundang/src/sign_up_page.dart';
+import 'package:flutter_bundang/src/screens/sign_up_page.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(
